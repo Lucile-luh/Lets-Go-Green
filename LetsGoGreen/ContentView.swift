cf8580de8cbb51577177b9da57ec1f09f9c7ce14
Lucile-luh <lucilemuchemwa374@gmail.com>
2025-10-16 12:24:35 +0200

    Initial Commit

---
+++ LetsGoGreen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LetsGoGreen
+//
+//  Created by LUCILE G MUCHEMWA on 16/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LetsGoGreen/LetsGoGreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsGoGreenApp.swift
+//  LetsGoGreen
+//
+//  Created by LUCILE G MUCHEMWA on 16/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct LetsGoGreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
