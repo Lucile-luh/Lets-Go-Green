fb9331065e8394b70559d06b54fae58727a4ffbc
Lucile-luh <lucilemuchemwa374@gmail.com>
2025-10-17 16:07:08 +0200

    Added textfields for the user to add in their credentials

--- LetsGoGreen/logInPage.swift
+++ LetsGoGreen/logInPage.swift
@@ -13,15 +13,45 @@ struct logInPage: View {
     
     var body: some View {
         
-        VStack {
+        NavigationStack {
             
-            Image( "appIconLTG")
-                .resizable()
-                .frame(width: 200, height: 200)
-                .aspectRatio(1,contentMode: .fit)
-            
+            ZStack {
+                LinearGradient(
+                    gradient: Gradient(colors: [
+                        Color(red: 0.95, green: 1.0, blue: 0.95), // Light mint green
+                        Color(red: 0.3, green: 0.85, blue: 0.3)   // Leafy green
+                    ]),
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
+                )
+                .ignoresSafeArea()
+                
+                VStack {
+                    Image( "appIconLTG")
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                        .aspectRatio(1,contentMode: .fit)
+                    
+
+                    TextField("Username", text: $username)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+//                        .background(.middlecolor.opacity(0.3))
+                        .foregroundStyle(.topColour)
+                        
+                        .cornerRadius(0.6)
+                        .padding()
+                       
+                    
+                    SecureField("Password", text: $Password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+//                        .background(.middlecolor.opacity(0.3))
+                        .foregroundStyle(.topColour)
+                        .padding()
+                }
+                
+            }
+//            .padding()
         }
-        .padding(20)
     }
 }
 
