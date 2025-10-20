ff19c48b0f32290d596601e75653156562f60eb2
Lucile-luh <lucilemuchemwa374@gmail.com>
2025-10-20 14:13:13 +0200

    added a join and create page

--- LetsGoGreen/joinAndCreatePage.swift
+++ LetsGoGreen/joinAndCreatePage.swift
@@ -9,7 +9,62 @@ import SwiftUI
 
 struct joinAndCreatePage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            ZStack {
+                LinearGradient(
+                    gradient: Gradient(colors: [
+                        Color(red: 0.95, green: 1.0, blue: 0.95), // Light mint green
+                        Color(red: 0.3, green: 0.85, blue: 0.3)   // Leafy green
+                    ]),
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
+                )
+                .ignoresSafeArea()
+                
+            Image(systemName: "figure.hiking")
+                    .foregroundStyle(.topColour)
+                    .font(.system(size: 80, weight: .bold, design: .default))
+                    .offset(x: 150, y: -270)
+                    .padding()
+                  
+                
+                VStack(spacing: 30) {
+                    NavigationLink(destination: joinEventPage()){
+                        Text("Join Event")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .fontDesign(.serif)
+                            .foregroundColor(.darker)
+                            .padding(.vertical, 22)
+                            .padding(.horizontal, 40)
+                            .background(Color.green.opacity(0.3))
+                            .cornerRadius(20)
+                    }
+                    .padding()
+                    
+                        NavigationLink(destination: createEventPage()){
+                            Text("Create Event")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                                .fontDesign(.serif)
+                                .foregroundColor(.darker)
+                                .padding(.vertical, 22)
+                                .padding(.horizontal, 40)
+                                .background(Color.green.opacity(0.3))
+                                .cornerRadius(20)
+                        }
+                    
+                    Image(systemName: "apple.meditate")
+                        .foregroundStyle(.topColour)
+                        .font(.system(size: 80, weight: .bold, design: .default))
+                        .offset(x: -140, y: 230)
+                        .padding()
+                      
+                        
+                    
+                }
+            }
+        }
     }
 }
 
